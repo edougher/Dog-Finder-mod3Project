4e5e883f221bc4504d94637b1ebb238283775efa
edougher <ejdougher@gmail.com>
2021-02-16 13:22:07 -0600

    nothing

--- backend-rails-api/db/seeds.rb
+++ backend-rails-api/db/seeds.rb
@@ -12,17 +12,6 @@ require 'json'
 require 'pry'
 require 'faker'
 
-#https://thedogapi.com/v1/images?api_key=
-#https://docs.thedogapi.com
-api_key = 'b381884d-c35c-45db-9b63-da84c9d8d80e'
-#url = 'https://api.thedogapi.com/v1/images?api_key=b381884d-c35c-45db-9b63-da84c9d8d80e'
-url = 'https://api.thedogapi.com/v1/images/search'
-response = RestClient.get(url)
-
-parsed_body = JSON.parse(response)
-binding.pry
-
-10.times 
 
 
 
