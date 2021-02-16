fc7b295c13d943974d71b836f34f1f096a91cc7f
edougher <ejdougher@gmail.com>
2021-02-16 11:12:11 -0600

    seed stuff

--- backend-rails-api/db/seeds.rb
+++ backend-rails-api/db/seeds.rb
@@ -16,5 +16,14 @@ api_key = 'b381884d-c35c-45db-9b63-da84c9d8d80e'
 #url = 'https://api.thedogapi.com/v1/images?api_key=b381884d-c35c-45db-9b63-da84c9d8d80e'
 url = 'https://api.thedogapi.com/v1/images/search'
 response = RestClient.get(url)
+
+parsed_body = JSON.parse(response)
 binding.pry
-JSON.parse(response)
+
+10.times 
+
+def functionName(parsed_body){
+    breedName = parsed_body[0]["name"]
+
+    Animal.create(name: breed image)
+}
