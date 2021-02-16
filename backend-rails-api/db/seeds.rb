1e3cf846ddf3b136b5944228f675fb7412871d87
velocichaptor <jake.m.chappelle@gmail.com>
2021-02-16 13:17:44 -0600

    users seed

--- backend-rails-api/db/seeds.rb
+++ backend-rails-api/db/seeds.rb
@@ -12,44 +12,41 @@ require 'json'
 require 'pry'
 require 'faker'
 
+Animal.destroy_all
+User.destroy_all
+
 #https://thedogapi.com/v1/images?api_key=
 #https://docs.thedogapi.com
 api_key = 'b381884d-c35c-45db-9b63-da84c9d8d80e'
-#url = 'https://api.thedogapi.com/v1/images?api_key=b381884d-c35c-45db-9b63-da84c9d8d80e'
-url = 'https://api.thedogapi.com/v1/images/search'
-response = RestClient.get(url)
-<<<<<<< HEAD
+# url = 'https://api.thedogapi.com/v1/images/search?api_key=b381884d-c35c-45db-9b63-da84c9d8d80e'
+# # url = 'https://api.thedogapi.com/v1/images/search'
+# response = RestClient.get(url)
 
-parsed_body = JSON.parse(response)
-binding.pry
+# parsed_body = JSON.parse(response)
 
-10.times 
-
-def functionName(parsed_body){
-    breedName = parsed_body[0]["name"]
-
-    Animal.create(name: breed image)
-}
-=======
-JSON.parse(response)
-binding.pry
+# parsed_body.each { |current_data|
+#     puts current_data   
+#     # breedName = current_data[0]["breeds"][0]["name"]
+#     # dogImage = current_data[0]["url"]
+#     # Animal.create(name: Faker::Creature::Dog.name, breed: breedName, image: dogImage)
+# }
 
 
+10.times do 
+    url = 'https://api.thedogapi.com/v1/images/search?api_key=b381884d-c35c-45db-9b63-da84c9d8d80e'
+    response = RestClient.get(url)
+    parsed_body = JSON.parse(response)
+    # binding.pry
+    # breedName = parsed_body[0]["breeds"][0]["name"]
+    dogImage = parsed_body[0]["url"]
+    Animal.create(name: Faker::Creature::Dog.name, breed: Faker::Creature::Dog.breed, image: dogImage)
+end
+
+5.times do
+    User.create(name: Faker::FunnyName.name, age: Faker::Number.within(range: 15..99), location: Faker::Movies::LordOfTheRings.location)
+end
 
 
+# Faker::Creature::Dog.breed
+# createDog(parsed_body){
 
-
-
-
-
-
-
-
-
-
-
-
-
-Faker::Creature::Dog.name
-Faker::Creature::Dog.age
->>>>>>> faker
