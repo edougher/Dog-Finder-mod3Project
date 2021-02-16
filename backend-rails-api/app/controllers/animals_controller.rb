b4db3d84675b0a604dac53f338385748bd3d8731
velocichaptor <jake.m.chappelle@gmail.com>
2021-02-16 15:06:46 -0600

    likes included in views

--- backend-rails-api/app/controllers/animals_controller.rb
+++ backend-rails-api/app/controllers/animals_controller.rb
@@ -23,8 +23,7 @@ class AnimalsController < ApplicationController
 
     def show
         animal = Animal.find(params[:id])
-
-        render json: animal
+        render json: animal, include: [:likes]
     end
   
     def update
