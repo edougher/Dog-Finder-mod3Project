24753cf1a418639f3fb062b785556c0f94bbf20b
edougher <ejdougher@gmail.com>
2021-02-19 13:03:04 -0600

    working sign up

--- backend-rails-api/app/controllers/users_controller.rb
+++ backend-rails-api/app/controllers/users_controller.rb
@@ -28,15 +28,17 @@ class UsersController < ApplicationController
     end
   
     def update
+
         user = User.find(params[:id])
         user.update(user_params)
+        binding.pry
         render json: user
     end
 
     private
 
     def user_params
-        params.require(:user).permit(:name, :total_score)
+        params.require(:user).permit(:name)
     end 
     
 
